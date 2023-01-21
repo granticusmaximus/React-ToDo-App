044691ca0d7edc87e251c047710d5358e7402e49
Grant Watson <grant@gwsapp.net>
2023-01-21 12:13:14 -0700

    Update to UI

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
-import ToDo from './ToDo.js'
-import 'bootstrap/dist/css/bootstrap.min.css';
+import Home from './TaskManager'
 
 function App() {
+
   return (
-    <div className="App">
-      <ToDo />
+    <div className='app'>
+      <Home />
     </div>
   );
 }
